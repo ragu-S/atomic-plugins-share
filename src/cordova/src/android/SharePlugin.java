b873940411c80f9a40cd18691a9b6ed3e90015f7
Ragu Sivanandha <ragu.sivanandha@gmail.com>
2016-02-23 17:27:17 -0500

    Added missing exception class

--- src/cordova/src/android/SharePlugin.java
+++ src/cordova/src/android/SharePlugin.java
@@ -20,6 +20,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLEncoder;
